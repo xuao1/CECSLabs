341720c1f9f3a44b4d1de21e003cf07f766c8a5a
xuao1 <xuao123@mail.ustc.edu.cn>
2023-10-23 21:11:02 +0800

    update

---
+++ simulator/testcases/functest/riscv32/src/max.d
@@ -0,0 +1,6 @@
+/home/xxa/Desktop/CECS-Lab/software/functest/build/riscv32/src/max.o: \
+ /home/xxa/Desktop/CECS-Lab/software/functest/src/max.c \
+ /home/xxa/Desktop/CECS-Lab/software/functest/include/trap.h \
+ ../base-port/base/include/base.h ../base-port/base/include/arch.h \
+ ../base-port/base/include/dev.h ../base-port/base/include/base-macro.h \
+ ../base-port/tool/include/tool.h
