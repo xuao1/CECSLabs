c3ae983f4f5a3e2e7523b9474ea72ba3bfb95890
xuao1 <xuao123@mail.ustc.edu.cn>
2023-10-30 19:54:32 +0800

    change build.mk

--- simulator/script/build.mk
+++ simulator/script/build.mk
@@ -1,7 +1,7 @@
 ARGS ?= 
 
 APP ?= hello
-APP_IMG = $(addprefix $(TEST_DIR)/testcases/, $(addsuffix -riscv32.bin, $(APP)))
+APP_IMG = $(addprefix $(TEST_DIR)/app/, $(addsuffix -riscv32.bin, $(APP)))
 IMG ?= $(APP_IMG)
 
 
