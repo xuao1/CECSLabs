c4143745e0b9b52911a83aaf33150fc93197dbe9
xuao1 <xuao123@mail.ustc.edu.cn>
2023-10-30 20:18:20 +0800

    update

--- software/base-port/base/src/rtc.c
+++ software/base-port/base/src/rtc.c
@@ -1,24 +1,26 @@
 #include <base.h>
 #include <dev-mmio.h>
 
+#define RTC_ADDR 0xA0000040
+
 void __timer_init() {
 }
 
 void __timer_uptime(DEV_TIMER_UPTIME_T *uptime) {
   // Lab6 TODO: implement uptime MMIO
   // hint: use function `inl` and RTC_ADDR to access RTC MMIO
-  uptime->lo = inl(RTC_ADDR);                  
-  uptime->hi = inl(RTC_ADDR + 4 * sizeof(uint32_t)); 
+    uint32_t low = inl(RTC_ADDR + 0);
+    uint32_t high = inl(RTC_ADDR + 4);
+    uptime->us = ((uint64_t)high << 32) | low;
 }
 
 void __timer_rtc(DEV_TIMER_RTC_T *rtc) {
   // Lab6 TODO: implement RTC MMIO
   // hint: use function `inl` and RTC_ADDR with offset  to access RTC MMIO
-    rtc->second = inl(RTC_ADDR + 8 * sizeof(uint32_t));  
-    rtc->minute = inl(RTC_ADDR + 12 * sizeof(uint32_t)); 
-    rtc->hour   = inl(RTC_ADDR + 16 * sizeof(uint32_t)); 
-    rtc->day    = inl(RTC_ADDR + 20 * sizeof(uint32_t)); 
-    rtc->month  = inl(RTC_ADDR + 24 * sizeof(uint32_t)); 
-    rtc->year   = inl(RTC_ADDR + 28 * sizeof(uint32_t)); 
-
+    rtc->second = inl(RTC_ADDR + 8);
+    rtc->minute = inl(RTC_ADDR + 12);
+    rtc->hour = inl(RTC_ADDR + 16);
+    rtc->day = inl(RTC_ADDR + 20);
+    rtc->month = inl(RTC_ADDR + 24);
+    rtc->year = inl(RTC_ADDR + 28);
 }
