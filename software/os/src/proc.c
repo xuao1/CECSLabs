c4856e6c8dbdfa77e85cf2e508395c49e909b242
xuao1 <xuao123@mail.ustc.edu.cn>
2023-11-02 10:41:13 +0800

    update

--- software/os/src/proc.c
+++ software/os/src/proc.c
@@ -16,7 +16,7 @@ static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 void init_proc() {
   Log("Initializing processes...");
   //load program here
-  // user_naive_load("/bin/menu");
+  user_naive_load("/bin/menu");
   user_naive_load("/bin/dummy");
 }
 
