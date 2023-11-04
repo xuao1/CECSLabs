4609fa812b1b966826f43cf69c318e38f0f0d273
xuao1 <xuao123@mail.ustc.edu.cn>
2023-11-04 14:31:21 +0800

    update

--- software/os/src/fs.c
+++ software/os/src/fs.c
@@ -41,40 +41,6 @@ void init_fs() {
   file_table[FD_DISPINFO].read = dispinfo_read;
 }
 
-// size_t serial_write(const void *buf, size_t offset, size_t len) {
-//   char *p = (char *)buf;
-//   for (int i = 0; i < len; i++) {
-//     putch(p[i]);
-//   }
-//   return len;
-// }
-
-// size_t keyboard_read(void *buf, size_t offset, size_t len) {
-//   DEV_INPUT_KEYBRD_T kbd = io_read(DEV_INPUT_KEYBRD);
-//   size_t read_len = 0;
-//   if (kbd.keydown) {
-//     read_len += sprintf(buf, "kd %s\n", keyname[kbd.keycode]);
-//   } else if (kbd.keycode != KEY_NONE) {
-//     read_len += sprintf(buf, "ku %s\n", keyname[kbd.keycode]);
-//   }
-//   return read_len;
-// }
-
-// size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-//   DEV_GPU_CONFIG_T gpu_config = io_read(DEV_GPU_CONFIG);
-//   return sprintf(buf, "WIDTH: %d\nHEIGHT: %d", gpu_config.width, gpu_config.height);
-// }
-
-// size_t fb_write(const void *buf, size_t offset, size_t len) {
-//   DEV_GPU_CONFIG_T gpu_config = io_read(DEV_GPU_CONFIG);
-//   offset /= 4;
-//   len /= 4;
-//   int x = offset % gpu_config.width;
-//   int y = offset / gpu_config.width;
-//   io_write(DEV_GPU_FBDRAW, x, y, (void*)buf, len, 1, true);
-//   return len;
-// }
-
 /* Open a file and return the size of file. */
 int fs_open(const char *pathname, int flags, int mode) {
   // Lab7 TODO: traverse the file_table and find the file
@@ -125,7 +91,8 @@ size_t fs_write(int fd, const void *buf, size_t len) {
     bytes_written = f->write(buf, 0, len);
   }
   else if(f->write == fb_write){
-    bytes_written = f->write(buf, 0, len);
+    bytes_written = f->write(buf, f->open_offset, len);
+    f->open_offset += bytes_written;
   }
   else{
     if(f->open_offset + len > f->size) len = f->size - f->open_offset;
