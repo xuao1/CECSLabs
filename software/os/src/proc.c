e772022da103cbf8f8c8513e21e1577603d9413f
xuao1 <xuao123@mail.ustc.edu.cn>
2023-11-04 14:48:44 +0800

    update

--- software/os/src/proc.c
+++ software/os/src/proc.c
@@ -17,7 +17,7 @@ void init_proc() {
   Log("Initializing processes...");
   //load program here
   // user_naive_load("/bin/menu");
-  user_naive_load("/bin/dummy");
+  // user_naive_load("/bin/dummy");
   user_naive_load("/bin/file-test");
 }
 
